77c5c6067bd1102eb9b641da5a68a778951afe92
SaiVinaylakkimsetti <120162249+SaiVinaylakkimsetti@users.noreply.github.com>
2023-01-09 17:21:52 +0530

    #include<stdio.h> int main() {     int n,m=0,i,temp,d;     scanf("%d",&n);     temp=n;     while(n!=0)     {         d=n%10;         n=n/10;         if(d==0)         {             continue;         }         m=m*10+d;     }     printf("%d",m); return 0; }

---
+++ Reverse_Integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,m=0,i,temp,d;
+    scanf("%d",&n);
+    temp=n;
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        if(d==0)
+        {
+            continue;
+        }
+        m=m*10+d;
+    }
+    printf("%d",m);
+return 0;
+}
